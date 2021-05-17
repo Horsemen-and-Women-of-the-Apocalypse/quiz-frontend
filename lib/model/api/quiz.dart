e612b37d91469c423f165cd955eb9f3fe6ffccaa
Thurstag <sashaberthout@hotmail.com>
2021-05-17 18:52:55 +0200

    [NEW] [SOLO] Add quiz wizard
    Add tests for quiz classes

--- lib/model/api/quiz.dart
+++ lib/model/api/quiz.dart
@@ -14,10 +14,47 @@ class QuizListItem {
   /// Load a QuizListItem from a JSON
   factory QuizListItem.fromJSON(Map<String, dynamic> json) {
     if (!json.containsKey(ID_FIELD_NAME) ||
-        !json.containsKey(NAME_FIELD_NAME)) {
+        !json.containsKey(NAME_FIELD_NAME) ||
+        !(json[ID_FIELD_NAME] is String) ||
+        !(json[NAME_FIELD_NAME] is String)) {
       throw Exception('Malformed QuizListItem');
     }
 
     return QuizListItem(json[ID_FIELD_NAME], json[NAME_FIELD_NAME]);
+  }
+}
+
+/// Abstract class for question
+abstract class AQuizQuestion {
+  /// Load a question from a JSON
+  static AQuizQuestion fromJSON(Map<String, dynamic> json) {
+    return StringMultipleChoiceQuestion.fromJSON(json);
+  }
+}
+
+/// Question with multiple choices defined by a string
+class StringMultipleChoiceQuestion extends AQuizQuestion {
+  static const String QUESTION_FIELD_NAME = 'question';
+  static const String ANSWERS_FIELD_NAME = 'answers';
+
+  final List<String> answers;
+  final String question;
+
+  /// Constructor
+  StringMultipleChoiceQuestion(String question, List<String> answers)
+      : question = question,
+        answers = answers.toList();
+
+  /// Load a StringMultipleChoiceQuestion from a JSON
+  factory StringMultipleChoiceQuestion.fromJSON(Map<String, dynamic> json) {
+    if (!json.containsKey(QUESTION_FIELD_NAME) ||
+        !json.containsKey(ANSWERS_FIELD_NAME) ||
+        !(json[QUESTION_FIELD_NAME] is String) ||
+        !(json[ANSWERS_FIELD_NAME] is List<String>)) {
+      throw Exception('Malformed StringMultipleChoiceQuestion');
+    }
+
+    return StringMultipleChoiceQuestion(
+        json[QUESTION_FIELD_NAME], json[ANSWERS_FIELD_NAME]);
   }
 }
---
+++ lib/widgets/quiz_form.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/model/api/quiz.dart';
+
+/// Quiz form widget
+class QuizForm extends StatefulWidget {
+  final void Function(QuizFormData) _onComplete;
+  final List<AQuizQuestion> _questions;
+
+  /// Constructor
+  QuizForm(
+      List<AQuizQuestion> questions, void Function(QuizFormData) onComplete)
+      : _questions = questions.toList(),
+        _onComplete = onComplete;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _QuizFormState(_questions, _onComplete);
+  }
+}
+
+/// Quiz form data
+class QuizFormData {
+  final List<Object?> answers;
+
+  /// Constructor
+  QuizFormData(List<AQuizQuestion> questions)
+      : answers = List.generate(questions.length, (index) => null);
+}
+
+class _QuizFormState extends State<QuizForm> {
+  final void Function(QuizFormData) _onComplete;
+  final List<AQuizQuestion> _questions;
+  final QuizFormData _form;
+  int _stepIndex = 0;
+
+  _QuizFormState(
+      List<AQuizQuestion> questions, void Function(QuizFormData) onComplete)
+      : _questions = questions.toList(),
+        _form = QuizFormData(questions),
+        _onComplete = onComplete;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stepper(
+      currentStep: _stepIndex,
+      physics: ClampingScrollPhysics(),
+      onStepCancel: () {
+        if (_stepIndex - 1 < 0) {
+          return;
+        }
+
+        setState(() {
+          _stepIndex--;
+        });
+      },
+      onStepContinue: () {
+        if (_stepIndex + 1 >= _questions.length) {
+          _onComplete(_form);
+          return;
+        }
+
+        setState(() {
+          _stepIndex++;
+        });
+      },
+      steps: Iterable.generate(
+          _questions.length, (i) => generateStep(_questions[i], i)).toList(),
+    );
+  }
+
+  /// Generate a step based on the given question
+  Step generateStep(AQuizQuestion question, int questionIndex) {
+    if (question is StringMultipleChoiceQuestion) {
+      return Step(
+          title: Text(question.question),
+          content: DropdownButton<Object>(
+              onChanged: (newValue) {
+                setState(() {
+                  _form.answers[questionIndex] = newValue;
+                });
+              },
+              value: _form.answers[questionIndex],
+              items: question.answers
+                  .map((e) => DropdownMenuItem(value: e, child: Text(e)))
+                  .toList()));
+    }
+
+    throw Exception('Unsupported question type: ${question.runtimeType}');
+  }
+}
