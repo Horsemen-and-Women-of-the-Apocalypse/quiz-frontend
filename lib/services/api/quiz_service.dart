80519567f23f42631f104c07b0b941eb33cac4b8
Remy.c <remychagnas@hotmail.fr>
2021-05-20 13:47:39 +0200

    Fix analyze

--- lib/services/api/quiz_service.dart
+++ lib/services/api/quiz_service.dart
@@ -1,4 +1,5 @@
 import 'package:quiz/model/api/quiz.dart';
+import 'package:quiz/model/creation/creation.dart';
 import 'package:quiz/services/api_service.dart';
 import 'package:quiz/widgets/quiz_form.dart';
 
@@ -22,7 +23,7 @@ class QuizService extends APIService {
   Future<SoloQuizResults> answer(String quizId, QuizFormData answers) async {
     return SoloQuizResults.fromJSON(await post('quiz/$quizId/answer', answers));
   }
-  
+
   /// Create the given quiz, returning its id
   Future<String> create(Quiz quiz) async {
     return await post('quiz/create', quiz) as String;
