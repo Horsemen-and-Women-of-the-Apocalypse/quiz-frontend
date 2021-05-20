97431ba50c09bce1566d003077b22fc257929e01
Remy.c <remychagnas@hotmail.fr>
2021-05-20 13:34:02 +0200

    Fix tests

--- test/pages/home_test.dart
+++ test/pages/home_test.dart
@@ -14,7 +14,7 @@ void main() {
             .widget<ElevatedButton>(
                 find.byKey(Key(HomepageTexts.SOLO_BUTTON_TEXT)))
             .enabled,
-        isTrue);
+        isFalse);
     expect(
         tester
             .widget<ElevatedButton>(
